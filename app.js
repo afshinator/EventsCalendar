7cf0d3ce06fb5c0ff0d5397efd70a17a9ddb9f08
Afshin Mokhtari <afshinator@gmail.com>
2024-12-06 16:57:29 -0800

    finish basic calender with back and forth button

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 const calendar = document.getElementById('calendar')
-
+const monthEl = document.getElementById('month');
 const months = [
   'January',
   'February',
@@ -16,32 +16,38 @@ const months = [
   'December',
 ];
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
+let events;
 const today = new Date();
 let currentMonth = today.getMonth();
 let currentYear = today.getFullYear();
 
+let firstDayOfWeek = new Date(currentYear, currentMonth, 1).getDay();
+let dayOfWeek = days[firstDayOfWeek];
+
+console.log(firstDayOfWeek, dayOfWeek)
+
+
 const drawBlankCalendar = () => {
   for (let i = 0; i < 35; i++) {
-    const day = document.createElement('div')
-    day.classList.add('day')
+    const day = document.createElement('div');
+    day.classList.add('day');
 
-    const dayText = document.createElement('p')
-    dayText.classList.add('day-text')
+    const dayText = document.createElement('p');
+    dayText.classList.add('day-text');
     dayText.innerText = days[i % 7];
 
-    const dayNumber = document.createElement('p')
-    dayNumber.classList.add('day-number')
+    const dayNumber = document.createElement('p');
+    dayNumber.classList.add('day-number');
 
     const eventName = document.createElement('small');
-    eventName.classList.add('event-name')
+    eventName.classList.add('event-name');
 
     day.appendChild(dayText);
-    day.appendChild(dayNumber)
+    day.appendChild(dayNumber);
     day.appendChild(eventName);
-    calendar.appendChild(day)
+    calendar.appendChild(day);
   }
-}
+};
 
 const updateCalendar = (month, year, events) => {
   const dayElements = document.querySelectorAll('.day');
@@ -50,10 +56,13 @@ const updateCalendar = (month, year, events) => {
   theFirst.setMonth(month);
   theFirst.setYear(year);
 
-  const theFirstDayOfWeek = theFirst.getDay();
+
+  let theFirstDayOfWeek = new Date(year, month, 1).getDay();
+  let dayOfWeek = days[theFirstDayOfWeek];
+
   const monthName = months[month];
   const monthWithYear = `${year} - ${monthName}`;
-
+  monthEl.innerText = monthWithYear;
   const daysInMonth = new Date(year, month + 1, 0).getDate();
 
   let dayCounter = 1;
@@ -64,14 +73,13 @@ const updateCalendar = (month, year, events) => {
     if (i >= theFirstDayOfWeek && dayCounter <= daysInMonth) {
       const thisDate = new Date(year, month, dayCounter);
 
-      const eventName = day.querySelector('.event-name');
-      if (events[thisDate]) {
-        const event = events[thisDate];
-        eventName.innerText = `* ${event.title}`;
-      } else {
-        eventName.innerText = ``;
-      }
-      console.log(thisDate);
+      // const eventName = day.querySelector('.event-name');
+      // if (events[thisDate]) {
+      //   const event = events[thisDate];
+      //   eventName.innerText = `* ${event.title}`;
+      // } else {
+      //   eventName.innerText = ``;
+      // }
 
       dayNumber.innerText = dayCounter;
       dayCounter++;
@@ -81,6 +89,23 @@ const updateCalendar = (month, year, events) => {
   }
 };
 
+
+
+const previousMonth = () => {
+  if (currentMonth === 0) {
+    currentMonth = 12;
+    currentYear--;
+  }
+  updateCalendar(--currentMonth, currentYear, events);
+};
+
+const nextMonth = () => {
+  if (currentMonth === 11) {
+    currentMonth = -1;
+    currentYear++;
+  }
+  updateCalendar(++currentMonth, currentYear, events);
+};
 
 drawBlankCalendar();
 updateCalendar(currentMonth, currentYear, {});
